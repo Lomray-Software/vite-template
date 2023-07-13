026edd93a9575c10e5b18df7ae6356ac71603545
Mikhail Yarmaliuk <hellboymxb@gmail.com>
2023-07-13 18:33:29 +0200

    fix: release version bump

--- release.config.js
+++ release.config.js
@@ -10,10 +10,19 @@ export default {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    ["@semantic-release/github", {
-      "assets": [
-        {"path": "build.zip", "label": "Build-${nextRelease.gitTag}"},
-      ]
-    }],
+    [
+      '@semantic-release/github',
+      {
+        assets: [
+          { path: 'build.zip', label: 'Build-${nextRelease.gitTag}' },
+        ]
+      }
+    ],
+    [
+      '@semantic-release/exec',
+      {
+        prepareCmd: 'npm version ${nextRelease.version} --no-git-tag-version',
+      }
+    ]
   ]
 }
