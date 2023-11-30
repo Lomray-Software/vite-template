4567b327aef95fd0f3a683ea089c70663fa79fcb
MatthewPattell <mikhail.yarmaliuk@lomray.com>
2023-11-30 12:48:06 +0100

    feat: aws amplify ssr deploy #3

--- amplify-build.sh
+++ amplify-build.sh
@@ -6,7 +6,9 @@ mkdir -p ./.amplify-hosting/compute/default
 
 cp -r ./build ./.amplify-hosting/compute/default/build
 cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
+cp -r ./package.json ./.amplify-hosting/compute/default/package.json
 
 cp -r ./build/client ./.amplify-hosting/static
+rm -r ./.amplify-hosting/compute/default/build/client/assets
 
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
