f7395e8be06a90aed6c37e1fdddd0875e3d95e01
MatthewPattell <mikhail.yarmaliuk@lomray.com>
2024-02-07 13:27:33 +0100

    chore: simplify IS_CLIENT constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 export const IS_SERVER = import.meta.env.SSR;
 
-export const IS_CLIENT = typeof window !== 'undefined';
+export const IS_CLIENT = !IS_SERVER;
 
 export const IS_PROD = import.meta.env.MODE === 'production';
 
